0c4cf12ba6da353fc99c19f395a1e641413d46e8
Aditya Lawate <aditya.211343101@vcet.edu.in>
2025-05-29 12:31:15 +0530

    created the logic of Course Class

--- src/Course.java
+++ src/Course.java
@@ -1,2 +1,21 @@
+import java.util.List;
+
 public class Course {
+    int courseID, credits;
+    String courseName;
+    Faculty faculty;
+    List<Student> enrolledStudents;
+    public void enrollStudent(Student student){
+        this.enrolledStudents.add(student);
+    }
+    public void removeStudent(Student student){
+        this.enrolledStudents.remove(student);
+    }
+    public void assignFaculty(Faculty fac){
+        this.faculty = fac;
+    }
+    public String getCourseDetails(){
+        return this.courseID+" "+this.courseName+" "+this.credits;
+    }
+
 }
